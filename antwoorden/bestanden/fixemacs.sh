530f20206f5cb645670b44c6a5f23e9ad1cdda0b
Kevin van der Vlist <kevin@kevinvandervlist.nl>
2011-06-22 10:29:03 +0200

    Fix emacs script.

--- antwoorden/bestanden/fixemacs.sh
+++ antwoorden/bestanden/fixemacs.sh
@@ -1,25 +1,26 @@
-% tirlnx01 - Materiaal om het keuzevak Linux te geven 
-% op de Hogeschool Rotterdam.
-% Copyright (C) 2010 - 2011  Paul Sohier, Kevin van der Vlist
-%
-% This program is free software: you can redistribute it and/or modify
-% it under the terms of the GNU General Public License as published by
-% the Free Software Foundation, either version 3 of the License, or
-% (at your option) any later version.
-%
-% This program is distributed in the hope that it will be useful,
-% but WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-% GNU General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License
-% along with this program.  If not, see <http://www.gnu.org/licenses/>.
-%
-% Kevin van der Vlist - kevin@kevinvandervlist.nl
-% Paul Sohier - paul@paulsohier.nl
-
 #!/bin/bash
 # Resolve de dependencies van emacs wanneer X niet is geinstalleerd. 
+# Slackware 31
+
+# tirlnx01 - Materiaal om het keuzevak Linux te geven 
+# op de Hogeschool Rotterdam.
+# Copyright (C) 2010 - 2011  Paul Sohier, Kevin van der Vlist
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Kevin van der Vlist - kevin@kevinvandervlist.nl
+# Paul Sohier - paul@paulsohier.nl
 
 if [[ $UID -ne 0 ]]; then
 	echo "Installeren doe je als root!"
